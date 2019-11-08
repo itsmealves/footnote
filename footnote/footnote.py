3735dd1c375c360706b51960fcd05c3a0c144b6b
Gabriel Alves <itsmealves@gmail.com>
2019-11-08 13:25:30 -0300

    backup

--- footnote/footnote.py
+++ footnote/footnote.py
@@ -2,7 +2,7 @@ import os
 import re
 import inspect
 import textwrap
-
+import types
 from functools import wraps
 from functools import reduce
 
@@ -31,7 +31,7 @@ class Footnote(ABC):
             full_text = original_text.replace('#', '').strip()
 
             def replace_args(t):
-                p = re.sub(r'\$\{.+\}', '{}', t)
+                p = re.sub(r'\$\{[\ \w \-\+\.\(\)\{\}]+\}', '{}', t)
                 q = re.sub(r'^\w+:', '', p).strip()
                 return q.replace('\'', '\\\'')
             def find_prefix(t):
@@ -41,7 +41,7 @@ class Footnote(ABC):
             def find_args(t):
                 return [
                     argument.replace('${', '').replace('}', '')
-                    for argument in re.findall(r'\$\{.+\}', t)
+                    for argument in re.findall(r'\$\{[\ \w \-\+\.\(\)\{\}]+\}', t)
                 ]
             
             comment_text = replace_args(full_text)
@@ -54,9 +54,10 @@ class Footnote(ABC):
             return cls.get_format(prefix, comment_text, *args) + os.linesep
         return re.sub(r'#.*\s', replace_fn, source)
 
-    @staticmethod
-    def remove_decorators(source):
-        return re.sub(r'@\w[\w\.]*', '', source)
+    @classmethod
+    def remove_decorator(cls, source):
+        return re.sub('@{}.inject'.format(cls.__name__), '', source)
+        # return re.sub(r'@\w[\w\.]*'.format(cls.__name__), '', source))
 
     @staticmethod
     def rename_function(source):
@@ -66,26 +67,46 @@ class Footnote(ABC):
     def inject(cls, fn, custom_context={}):
         transforms = [
             cls.replace_comments,
-            cls.remove_decorators,
-            cls.rename_function,
+            cls.remove_decorator,
             cls.normalize_indentation
         ]
 
-        local = {}
+        local_context = {}
         source = inspect.getsource(fn)
         patched_source = reduce(lambda a, b: b(a), transforms, source)
-        
         context = {**fn.__globals__, **custom_context, **cls.get_context()}
-        exec(patched_source, context, local)
-        patched_fn = local.get('patched_fn')
 
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-            return patched_fn(*args, **kwargs)
-        return wrapper
+        compiled_fn = compile(patched_source, inspect.getsourcefile(fn), 'exec')
+        exec(compiled_fn, context, local_context)
+
+        return wraps(fn)(local_context.get(fn.__name__))
 
     @classmethod
-    def spread(cls, inject_cls):
-        for _, fn in inspect.getmembers(inject_cls, predicate=inspect.isfunction):
-            cls.inject(fn, { inject_cls.__name__: inject_cls })
-        return inject_cls
+    def spread(cls, global_data):
+        def spread_fn(inject_cls):
+
+            def patch_mangle(name):
+                prefix = '_' + inject_cls.__name__
+                if name.startswith(prefix):
+                    return name.replace(prefix, '')
+                return name
+
+            def patch_members(member, class_def):
+                if type(member) == staticmethod:
+                    member = member.__func__
+                return cls.inject(member, { inject_cls.__name__: class_def, **global_data })
+
+            body = {
+                patch_mangle(member_name): member
+                for member_name, member in inject_cls.__dict__.items()
+                if not inspect.isroutine(member)
+            }
+
+            class_def = type(inject_cls.__name__, inspect.getmro(inject_cls)[1:], body)
+
+            for name, member in inject_cls.__dict__.items():
+                if inspect.isroutine(member):
+                    setattr(class_def, name, patch_members(member, class_def))
+
+            return class_def
+        return spread_fn
